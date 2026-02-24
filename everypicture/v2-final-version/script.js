a5ecb7d55623347dcd7bea1b0b63e8b26d17484a
brentpark8766@gmail.com <brepark@ucdavis.edu>
2026-02-24 13:22:35 -0800

    every picture final

---
+++ everypicture/v2-final-version/script.js
@@ -0,0 +1,56 @@
+(function() {
+
+    "use strict";
+    console.log("reading js");
+
+    const img = document.querySelector("img")
+    const title = document.querySelector("#title");
+    const story = document.querySelector(".story");
+
+    const artworks = [
+        {src: "images/cartoon.jpg", title: "The Medley", story: "The artwork to represent the start of my journey, and one that captures the childhood energy of colorful doodles and playful characters."}, 
+        {src: "images/mononoke.jpg", title: "Mononoke", story: "A shift into structured imagination. My use of color dissipates, signaling the change to a mature study of achromatic shading."}, 
+        {src: "images/rose.jpg", title: "Severance", story: "The work that marks my commitment to precise realism. Detailed observations clash with creativity, and technique becomes the focus."}, 
+        {src: "images/figure.jpg", title: "Study of Form", story: "This piece represents the peak of my matured study in studio art. My artistic development prioritizes form, proportions, and texture over imagination."}, 
+        {src: "images/watercolor.jpg", title: "Metamorphosis", story: "A return to what was once a colorful palette and playful compostions. This painting completes the cycle of my artistic style."}
+    ];
+
+    let current = 0;
+    let startTimer = false;
+
+    img.addEventListener("mousemove", (e) => {
+        const position = img.getBoundingClientRect();
+
+        const centerX = position.left + position.width / 2;
+        const centerY = position.top + position.height / 2;
+
+        const mouseDistanceX = e.clientX - centerX;
+        const mouseDistanceY = e.clientY - centerY;
+
+        const distance = Math.hypot(mouseDistanceX, mouseDistanceY);
+        const maxDistance = Math.hypot(position.width / 2, position.height / 2);
+        const percentage = Math.min(distance / maxDistance, 1);
+        
+        const blurValue = percentage * 14
+        img.style.filter = `blur(${blurValue}px)`;
+
+        if (blurValue < 1 && !startTimer) {
+            startTimer = true;
+
+            setTimeout (function () {
+                current = (current + 1) % artworks.length;
+
+                setTimeout (function () {
+                    img.src = artworks[current].src;
+                    title.textContent = artworks[current].title;
+                    story.textContent = artworks[current].story;
+
+                    img.style.filter = "blur(14px)";
+
+                    startTimer = false;
+                }, 500);
+            }, 2000); 
+        }
+    });
+
+}());
