71b50e3c6001b98c99f9114aa99c4e1cac2ac86a
brentpark8766@gmail.com <brepark@ucdavis.edu>
2026-03-05 02:21:34 -0800

    every picture improvements done

---
+++ everypicture/improvements/script.js
@@ -0,0 +1,80 @@
+(function() {
+
+    "use strict";
+    console.log("reading js");
+
+    const img = document.querySelector("img")
+    const title = document.querySelector("#title");
+    const story = document.querySelector(".story");
+
+    // array stores all the elements that get changed through the function
+    const artworks = [
+        {src: "images/cartoon.jpg", title: "The Medley", story: "The artwork to represent the start of my journey, and one that captures the childhood energy of colorful doodles and playful characters."}, 
+        {src: "images/mononoke.jpg", title: "Mononoke", story: "A shift into structured imagination. My use of color dissipates, signaling the change to a mature study of achromatic shading."}, 
+        {src: "images/rose.jpg", title: "Severance", story: "The work that marks my commitment to precise realism. Detailed observations clash with creativity, and technique becomes the focus."}, 
+        {src: "images/figure.jpg", title: "Study of Form", story: "This piece represents the peak of my matured study in studio art. My artistic development prioritizes form, proportions, and texture over imagination."}, 
+        {src: "images/watercolor.jpg", title: "Metamorphosis", story: "A return to what was once a colorful palette and playful compostions. This painting completes the cycle of my artistic style."}
+    ];
+
+    let current = 0;
+    let startTimer = false;
+    let focused = false;
+
+    // function that allows mouse position to focus the artwork
+    img.addEventListener("mousemove", function(e) {
+        // get the positional elements 
+        const position = img.getBoundingClientRect();
+
+        const centerX = position.left + position.width / 2;
+        const centerY = position.top + position.height / 2;
+
+        const mouseDistanceX = e.clientX - centerX;
+        const mouseDistanceY = e.clientY - centerY;
+        
+        // find distance of mouse from center
+        const distance = Math.hypot(mouseDistanceX, mouseDistanceY);
+
+        // find largest possible distance from center
+        const maxDistance = Math.hypot(position.width / 2, position.height / 2);
+
+        // converts distance to a percentage of max distance to use as a scale
+        const percentage = Math.min(distance / maxDistance, 1);
+        
+        // calculate how much to blur image based on distance calcs
+        const blurValue = percentage * 14
+        img.style.filter = `blur(${blurValue}px)`;
+
+        // update state of focused
+        focused = blurValue < 1;
+    });
+
+    // reset status of photo if the mouse is moved away
+    img.addEventListener("mouseleave", function() {
+        img.style.filter = "blur(14px)";
+        focused = false;
+    });
+
+    // function that integrates the element change once the image is focused and clicked on
+    img.addEventListener("click", function() {
+
+        // stops function if the image isn't focused or the timer hasn't started
+        if (!focused || startTimer) return;
+            startTimer = true;
+
+            setTimeout (function () {
+                // goes through the array and ensures cycle loop
+                current = (current + 1) % artworks.length;
+
+                // change elements
+                img.src = artworks[current].src;
+                title.textContent = artworks[current].title;
+                story.textContent = artworks[current].story;
+
+                // reset blur and timer
+                img.style.filter = "blur(14px)";
+                startTimer = false; 
+                focused = false;
+            }, 10); // pretty irrelevant timing, but I kept it since it was an example technique from the slides
+    });
+
+}());
